e7c13401c35bfb42a597c6c489180591d9e1af94
Roopan <roopant12@gmail.com>
2024-08-19 07:00:41 +0530

    PatternA program issue fixed

--- PatternProgramming/src/LaunchP1/PatternA.java
+++ PatternProgramming/src/LaunchP1/PatternA.java
@@ -6,7 +6,7 @@ public class PatternA {
 		int n=5;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				if(i==0 || j==0 || j==n-1 || i==(n-1)/2) {
+				if(i==0&&j>0&&j<n-1 || j==0&&i>0 || j==n-1&&i>0 || i==(n-1)/2) {
 					System.out.print("*");
 				}else {
 					System.out.print(" ");
